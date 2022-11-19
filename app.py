17fffb5454c7de85946839ffa0860a131e22a083
Markiian <markiyanpavlychak@gmail.com>
2022-11-19 15:03:32 +0200

    added hashing passwords

--- app.py
+++ app.py
@@ -1,10 +1,11 @@
 from flask import Flask, render_template, flash, request
 from flask_wtf import FlaskForm
-from wtforms import StringField, SubmitField
-from wtforms.validators import DataRequired
+from wtforms import StringField, SubmitField, PasswordField, BooleanField, ValidationError
+from wtforms.validators import DataRequired, EqualTo, Length
 from flask_sqlalchemy import SQLAlchemy
 from flask_migrate import Migrate
 from datetime import datetime
+from werkzeug.security import generate_password_hash, check_password_hash
 
 
 app = Flask(__name__)
@@ -17,24 +18,47 @@ db = SQLAlchemy(app)
 migrate = Migrate(app, db)
 
 # Create Model
+
+
 class Users (db.Model):
     id = db.Column(db.Integer, primary_key=True)
-    name =db.Column(db.String(200), nullable=False)
-    email =db.Column(db.String(120), nullable=False, unique=True)
+    name = db.Column(db.String(200), nullable=False)
+    email = db.Column(db.String(120), nullable=False, unique=True)
     favorite_color = db.Column(db.String(120))
-    date_added =db.Column(db.DateTime, default=datetime.utcnow)
+    date_added = db.Column(db.DateTime, default=datetime.utcnow)
+    # Do some password stuff
+    password_hash = db.Column(db.String(128))
+
+    @property
+    def password(self):
+        raise AttributeError('password is not a readable attribute!')
+
+    @password.setter
+    def password(self, password):
+        self.password_hash = generate_password_hash(password)
+
+    def verify_password(self, password):
+        return check_password_hash(self.password_hash, password)
 
     def __repr__(self):
-        return' <Name %r>' % self.name
+        return ' <Name %r>' % self.name
 
 #
+
+
 class UserForm(FlaskForm):
     name = StringField("Name", validators=[DataRequired()])
     email = StringField("Email", validators=[DataRequired()])
     favorite_color = StringField("Favorite Color")
+    password_hash = PasswordField('Password', validators=[DataRequired(), EqualTo(
+        'password_hash2', message='Passwords must match!')])
+    password_hash2 = PasswordField(
+        'Confirm Password', validators=[DataRequired()])
     submit = SubmitField("Submit")
 
 # Create a class
+
+
 class NamerForm(FlaskForm):
     name = StringField("What's Your Name", validators=[DataRequired()])
     submit = SubmitField("Submit")
@@ -52,11 +76,15 @@ def user(name):
 
 # Custom error pages
 # invalid url
+
+
 @app.errorhandler(404)
 def page_not_found(e):
     return render_template('404.html'), 404
 
 # internal server error
+
+
 @app.errorhandler(500)
 def page_not_found(e):
     return render_template('500.html'), 500
@@ -64,25 +92,29 @@ def page_not_found(e):
 
 @app.route('/user/add', methods=['GET', 'POST'])
 def add_user():
-    name=None
+    name = None
     form = UserForm()
     if form.validate_on_submit():
         user = Users.query.filter_by(email=form.email.data).first()
         if user is None:
-            user = Users(name=form.name.data, 
-                         email = form.email.data,
-                         favorite_color = form.favorite_color.data)
+            # hash password
+            hashed_pw = generate_password_hash(form.password_hash.data, 'sha256')
+            user = Users(name=form.name.data,
+                         email=form.email.data,
+                         favorite_color=form.favorite_color.data,
+                         password_hash=hashed_pw)
             db.session.add(user)
             db.session.commit()
         name = form.name.data
-        form.name.data=''
-        form.email.data=''
-        form.favorite_color.data=''
+        form.name.data = ''
+        form.email.data = ''
+        form.favorite_color.data = ''
+        form.password_hash.data = ''
         flash('User added successfully!')
     our_users = Users.query.order_by(Users.date_added)
-    return render_template('add_user.html', form=form, 
-                                            name=name, 
-                                            our_users=our_users)
+    return render_template('add_user.html', form=form,
+                           name=name,
+                           our_users=our_users)
 
 # Create name page
 
@@ -102,10 +134,11 @@ def name():
 
 # Update db record
 
+
 @app.route('/update/<int:id>', methods=['GET', 'POST'])
-def update (id):
+def update(id):
     form = UserForm()
-    name_to_update = Users.query.get_or_404(id) 
+    name_to_update = Users.query.get_or_404(id)
     if request.method == 'POST':
         name_to_update.name = request.form['name']
         name_to_update.email = request.form['email']
@@ -113,15 +146,37 @@ def update (id):
         try:
             db.session.commit()
             flash("User Updated successfully")
-            return render_template ("update.html", form=form,
-                            name_to_update = name_to_update)
+            return render_template("update.html", form=form,
+                                   name_to_update=name_to_update)
         except:
             flash("Error! Looks like there was a problem, try again")
-            return render_template ("update.html", form=form,
-                            name_to_update = name_to_update)
+            return render_template("update.html", form=form,
+                                   name_to_update=name_to_update,
+                                   id=id)
     else:
-        return render_template ("update.html", form=form,
-                            name_to_update = name_to_update)
+        return render_template("update.html", form=form,
+                               name_to_update=name_to_update,
+                               id=id)
+
+
+@app.route('/delete/<int:id>')
+def delete(id):
+    user_to_delete = Users.query.get_or_404(id)
+    name = None
+    form = UserForm()
+    try:
+        db.session.delete(user_to_delete)
+        db.session.commit()
+        flash("User deleted Successfully!")
+        our_users = Users.query.order_by(Users.date_added)
+        return render_template('add_user.html', form=form,
+                               name=name,
+                               our_users=our_users)
+    except:
+        flash("Whoops! There was a problem, try again!")
+        return render_template('add_user.html', form=form,
+                               name=name,
+                               our_users=our_users)
 
 
 if __name__ == "__main__":
